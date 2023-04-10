a8d61c5e220ecc8369d6dca1b0e19ca19dbd1992
crimefightingllama <92220266+crimefightingllama@users.noreply.github.com>
2023-04-10 20:05:50 +0100

    missed one

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -783,7 +783,7 @@ def refine_detections_graph(rois, probs, deltas, window, config):
     return detections
 
 
-class DetectionLayer(KE.Layer):
+class DetectionLayer(KE.base_layer.Layer):
     """Takes classified proposal boxes and their bounding box deltas and
     returns the final detection boxes.
 
